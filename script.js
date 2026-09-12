0ddb97c5beba3738ca84ba6a46ab1c61426c29f7
Boyanapalli Yogendra sai <boyanapalliyogendrasai@gmail.com>
2026-09-12 10:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,32 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
+	const romanpairs=[
+		['M',1000],
+		['CM',900],
+		['D',500],
+		['CD',400],
+		['C',100],
+		['XC',90],
+		['L',50],
+		['XL',40],
+		['X',10],
+		['IX',9],
+		['V',5],
+		['IV',4],
+		['I',1].
+	];
+	let result="";
+	for(const [symbol,value] of romanpairs)
+		{
+			while(num>=value)
+				{
+					result +=symbol;
+					num-=value;
+				}
+		}
+	return result;
+
   //your code here
 
 }
